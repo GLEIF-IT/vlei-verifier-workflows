54b0941c5cb9e4faf3c71bdcd01f4e6da7b37c37
Jonathan Rayback <jrayback@gmail.com>
2025-03-07 12:05:31 -0700

    simplify  external api
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
-export { generateFileDigest } from './utils/generate-digest.js';
-export { buildTestData, EcrTestData } from './utils/generate-test-data.js';
-export {
-  buildCredentials,
-  buildAidData,
-  User,
-  CredentialInfo,
-} from './utils/handle-json-config.js';
-export { WorkflowRunner } from './utils/run-workflow.js';
-export {
-  getConfig,
-  getGrantedCredential,
-  VleiUser,
-  loadWorkflow,
-} from './utils/test-data.js';
+import { WorkflowRunner } from './utils/run-workflow.js';
+import { getConfig, loadWorkflow } from './utils/test-data.js';
+import { StepRunner } from './utils/workflow-step-runners.js';
 
-export {
+export { WorkflowRunner } from './utils/run-workflow.js';
+export { getConfig, loadWorkflow } from './utils/test-data.js';
+export { StepRunner } from './utils/workflow-step-runners.js';
+
+export default {
+  WorkflowRunner,
+  getConfig,
+  loadWorkflow,
   StepRunner,
-  IssueCredentialStepRunner,
-  RevokeCredentialStepRunner,
-  CreateAidStepRunner,
-  CreateClientStepRunner,
-  CreateRegistryStepRunner,
-  AddRootOfTrustStepRunner,
-  NotifyCredentialIssueeStepRunner,
-  CredentialVerificationStepRunner,
-} from './utils/workflow-step-runners.js';
-export { WorkflowState } from './workflow-state.js';
-export { VleiIssuance } from './vlei-issuance.js';
-export { getOrCreateClients } from './utils/test-util.js';
+};
