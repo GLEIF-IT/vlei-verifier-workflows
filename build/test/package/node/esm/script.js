3a840c7f80258e563a5b6522cab1cbecfb7cfc4c
Jonathan Rayback <jrayback@gmail.com>
2025-03-08 14:50:07 -0700

    add check for named imports
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- build/test/package/node/esm/script.js
+++ build/test/package/node/esm/script.js
@@ -1,9 +1,21 @@
 import * as workflow from '@simple-ssi/vlei-verifier-workflows';
+import {
+  WorkflowRunner,
+  StepRunner,
+  getConfig,
+  loadWorkflow,
+} from '@simple-ssi/vlei-verifier-workflows';
 
-// just make sure the imports are working
-const workflowRunnerCtor = workflow.WorkflowRunner;
-const stepRunnnerCtor = workflow.StepRunner;
-const getConfigFunc = workflow.getConfig;
-const loadWorkflowFunc = workflow.loadWorkflow;
+// make sure namespace imports are working
+let workflowRunnerCtor = workflow.WorkflowRunner;
+let stepRunnnerCtor = workflow.StepRunner;
+let getConfigFunc = workflow.getConfig;
+let loadWorkflowFunc = workflow.loadWorkflow;
+
+// make sure named imports are working
+workflowRunnerCtor = WorkflowRunner;
+stepRunnnerCtor = StepRunner;
+getConfigFunc = getConfig;
+loadWorkflowFunc = loadWorkflow;
 
 console.log('\nesm ok');
