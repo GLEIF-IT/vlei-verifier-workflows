a159113fd4fe2cf6e90ca7a21665e59165ee4d5a
Aidar Negimatzhanov <aydar.negimatzhanov@perfectart.com>
2025-04-29 14:30:26 +0200

    Bump package versions to 0.0.8-dev3 and 0.0.9, and export additional modules

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { WorkflowRunner } from './utils/run-workflow.js';
 import { getConfig, loadWorkflow } from './utils/test-data.js';
 import { StepRunner } from './utils/workflow-step-runners.js';
+export { resolveEnvironment, TestEnvironment } from './utils/resolve-env.js';
 
 export { WorkflowRunner } from './utils/run-workflow.js';
 export { getConfig, loadWorkflow } from './utils/test-data.js';
 export { StepRunner } from './utils/workflow-step-runners.js';
+export { WorkflowState } from './workflow-state.js';
 
 export default {
   WorkflowRunner,
   getConfig,
   loadWorkflow,
-  StepRunner,
+  StepRunner
 };
